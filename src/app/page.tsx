85b433973ca50704c4b453cf17aa00d7ba89025f
Adrian Cubillos <adriancubillos@gmail.com>
2024-11-15 13:02:19 -0500

    Added React icons library and added a smiley to a nextjs buton

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
-import Image from "next/image";
+import { Button } from "@nextui-org/react";
+import { GoSmiley } from "react-icons/go";
 
 export default function Home() {
   return (
     <div>
       <h1 className="text-3xl">Hello Adri </h1>
-
+      <Button color="danger" variant="bordered" startContent={<GoSmiley />}>Clcik me</Button>
     </div>
   );
 }
