0ec9f99d0029dd4e932a0d9ab68505896e762bc3
Adrian Cubillos <adriancubillos@gmail.com>
2024-11-15 17:59:02 -0500

    Added use of zod schemas for validation to login form and created register schema, form and page

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -3,7 +3,7 @@ import LoginForm from './LoginForm'
 
 export default function LoginPage() {
   return (
-    <div>
+    <div className="flex items-center justify-center vertical-center">
       <LoginForm />
     </div>
   )
--- src/app/(auth)/register/RegisterForm.tsx
+++ src/app/(auth)/register/RegisterForm.tsx
@@ -1,40 +1,43 @@
 "use client";
 import {
-  Button,
+  RegisterSchema,
+  registerSchema,
+} from "@/lib/schemas/RegisterSchema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
   Card,
-  CardBody,
   CardHeader,
+  CardBody,
+  Button,
   Input,
 } from "@nextui-org/react";
 import React from "react";
+import { useForm } from "react-hook-form";
 import { GiPadlock } from "react-icons/gi";
-import { useForm, SubmitHandler } from "react-hook-form"
-import { loginSchema, LoginSchema } from "@/lib/schemas/LoginSchema";
-import { zodResolver } from "@hookform/resolvers/zod";
-
-
-export default function LoginForm() {
-  const { register, handleSubmit, formState: { isValid, errors } } = useForm<LoginSchema>(
-    {
-      resolver: zodResolver(loginSchema),
-      mode: "onTouched"
-    }
-  );
-  const onSubmit = (data: LoginSchema) => {
-    console.log("Form submitted", data);
+export default function RegisterForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid, isSubmitting },
+  } = useForm<RegisterSchema>({
+    resolver: zodResolver(registerSchema),
+    mode: "onTouched",
+  });
+  const onSubmit = (data: RegisterSchema) => {
+    console.log(data);
   };
   return (
-    <Card className="w-2/5 mx-auto">
+    <Card className="w-3/5 mx-auto">
       <CardHeader className="flex flex-col items-center justify-center">
         <div className="flex flex-col gap-2 items-center text-default">
           <div className="flex flex-row items-center gap-3">
             <GiPadlock size={30} />
             <h1 className="text-3xl font-semibold">
-              Login
+              Register
             </h1>
           </div>
           <p className="text-neutral-500">
-            Welcome back to MatchMe!
+            Welcome to NextMatch
           </p>
         </div>
       </CardHeader>
@@ -43,11 +46,19 @@ export default function LoginForm() {
           <div className="space-y-4">
             <Input
               defaultValue=""
+              label="Name"
+              variant="bordered"
+              {...register("name")}
+              isInvalid={!!errors.name}
+              errorMessage={errors.name?.message}
+            />
+            <Input
+              defaultValue=""
               label="Email"
               variant="bordered"
               {...register("email")}
-              isInvalid={!!errors.email} //!! is the double negation operator. It's used to convert a value to a boolean.
-              errorMessage={errors.email?.message as string}
+              isInvalid={!!errors.email}
+              errorMessage={errors.email?.message}
             />
             <Input
               defaultValue=""
@@ -56,16 +67,18 @@ export default function LoginForm() {
               type="password"
               {...register("password")}
               isInvalid={!!errors.password}
-              errorMessage={errors.password?.message as string}
-
+              errorMessage={
+                errors.password?.message
+              }
             />
             <Button
+              isLoading={isSubmitting}
+              isDisabled={!isValid}
               fullWidth
               color="default"
               type="submit"
-              isDisabled={!isValid}
             >
-              Login
+              Register
             </Button>
           </div>
         </form>
